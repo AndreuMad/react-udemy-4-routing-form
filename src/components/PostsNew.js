37c7c7d64a89c099e639c420a079d0a7ef67d35b
Andrii Kit <andrii.labyk@gmail.com>
2017-07-02 14:06:40 +0300

    u

--- src/components/PostsNew.js
+++ src/components/PostsNew.js
@@ -1,25 +1,41 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import classNames from 'classnames';
 
 class PostsNew extends Component {
 
     renderField(field) {
+        const { meta: { touched, error } } = field;
+
         return (
-            <div className="form-group">
+            <div className={
+                classNames(
+                    'form-group',
+                    { 'has-danger': touched && error},
+                )
+            }>
                 <label>{field.label}</label>
                 <input
                     className="form-control"
                     type="text"
                     {...field.input}
                 />
-                {field.meta.error}
+                <div className="text-help">
+                    {touched ? error : ''}
+                </div>
             </div>
         );
     }
 
+    onSubmit(values) {
+        console.log(values);
+    }
+
     render() {
+        const { handleSubmit } = this.props;
+
         return (
-            <form>
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
                 <Field
                     label="Title"
                     name="title"
@@ -35,6 +51,7 @@ class PostsNew extends Component {
                     name="content"
                     component={this.renderField}
                 />
+                <button type="submit" className="btn btn-primary">Submit</button>
             </form>
         );
     }
