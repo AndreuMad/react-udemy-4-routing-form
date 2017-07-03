63bfddf46b51b73b7fefb7b0980659f737ea442c
Andrii Kit <andrii.labyk@gmail.com>
2017-07-03 23:06:02 +0300

    u

--- src/components/PostsShow.js
+++ src/components/PostsShow.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchPost } from '../actions';
 
 class PostsShow extends Component {
     componentDidMount() {
-        console.log(this.props);
-        this.props.fetchPost(this.props.match.params.id);
+        if(!this.props.post) {
+            this.props.fetchPost(this.props.match.params.id);
+        }
     }
 
     render() {
+        const { post } = this.props;
+
+        if(!post) {
+            return (
+                <div>Loading...</div>
+            );
+        }
+
         return (
             <div>
-                Posts Show!
+                <Link to='/'>Back To Index</Link>
+                <h3>{post.title}</h3>
+                <h6>Categories: {post.categories}</h6>
+                <p>{post.content}</p>
             </div>
         );
     }
