01fabfd7662161af78a95198ee62fa6cc2d7c515
Andrii Kit <andrii.labyk@gmail.com>
2017-07-04 21:55:28 +0300

    u

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 export const FETCH_POSTS = 'fetch_posts';
 export const CREATE_POST = 'create_post';
 export const FETCH_POST = 'fetch_post';
+export const DELETE_POST = 'delete_post';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=andreumad';
@@ -21,10 +22,19 @@ export function fetchPosts() {
 export function fetchPost(id) {
     const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
 
-    return {
-        type: FETCH_POST,
-        payload: request
+    return (dispatch) => {
+        request.then((request) => {
+            dispatch({
+                type: FETCH_POST,
+                payload: request
+            });
+        });
     };
+
+    // return {
+    //     type: FETCH_POST,
+    //     payload: request
+    // };
 }
 
 //
@@ -40,3 +50,13 @@ export function createPost(values, callback) {
 }
 
 //
+
+export function deletePost(id, callback) {
+    const request = axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`)
+        .then(() => callback());
+
+    return {
+        type: DELETE_POST,
+        payload: request.id
+    };
+}
--- src/components/PostsShow.js
+++ src/components/PostsShow.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { fetchPost } from '../actions';
+import { fetchPost, deletePost } from '../actions';
 
 class PostsShow extends Component {
     componentDidMount() {
         if(!this.props.post) {
             this.props.fetchPost(this.props.match.params.id);
         }
+    }
+
+    onDeleteClick() {
+        this.props.deletePost(this.props.match.params.id, () => {
+            this.props.history.push('/');
+        });
     }
 
     render() {
@@ -22,6 +28,12 @@ class PostsShow extends Component {
         return (
             <div>
                 <Link to='/'>Back To Index</Link>
+                <button
+                    className="btn btn-danger pull-right"
+                    onClick={this.onDeleteClick.bind(this)}
+                >
+                    Delete Post
+                </button>
                 <h3>{post.title}</h3>
                 <h6>Categories: {post.categories}</h6>
                 <p>{post.content}</p>
@@ -34,4 +46,4 @@ function mapStateToProps({ posts }, ownProps) {
     return { post: posts[ownProps.match.params.id] };
 }
 
-export default connect(mapStateToProps, { fetchPost })(PostsShow)
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostsShow)
